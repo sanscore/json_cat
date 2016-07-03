c6bc8c7e2ba81b9d6baccbec01cea4091ef17171
Grant Welch <gwelch925@gmail.com>
2016-07-03 13:15:49 -0700

    Kernel::bow_print takes var-args.

--- lib/json_cat/ext/kernel.rb
+++ lib/json_cat/ext/kernel.rb
@@ -1,7 +1,11 @@
 # encoding: utf-8
 
 module Kernel
-  def bow_print(obj)
-    puts obj.rainbow_print
+  def bow_print(*objs)
+    objs.each do |obj|
+      puts obj.rainbow_print
+    end
+    objs.size <= 1 ? objs.first : objs
   end
+  module_function :bow_print
 end
